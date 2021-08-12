19eb9e84040704b689bf7a299c0f9a1d7cd5738f
Nicholas Dechert <dechert.nicholas@gmail.com>
2021-08-12 06:25:01 -0700

    Fixes $11w, world lighting is only wrong on reload in the editor, not the build

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,7 +10,7 @@ public class MainMenu : MonoBehaviour
         // We sometimes come from the game, where the cursor is locked
         Cursor.lockState = CursorLockMode.None;
     }
-    
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Main World");
